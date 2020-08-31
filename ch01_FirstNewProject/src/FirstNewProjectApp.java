f449b3b43108509429df18b514ae0581ab3e44b7
Josh Motta <joshua.a.motta@gmail.com>
2020-08-31 18:28:44 -0400

    init commit

---
+++ ch01_FirstNewProject/src/FirstNewProjectApp.java
@@ -0,0 +1,16 @@
+/* 
+ * My First Project
+ * August 29, 2020
+ */
+
+
+public class FirstNewProjectApp {
+
+	public static void main(String[] args) {
+		
+		System.out.print("Hello ");
+		System.out.println("there!");
+		
+	}
+
+}
---
+++ ch01_Test/src/TestApp.java
@@ -0,0 +1,14 @@
+/* 
+ *  Exercise 1-2: Use Eclipse to develop an application
+ *  August 29, 2020
+ */
+
+public class TestApp {
+
+	public static void main(String[] args) {
+		System.out.println("Success!");
+		System.out.println("Success!");
+		System.out.println("Success!");
+	}
+
+}
