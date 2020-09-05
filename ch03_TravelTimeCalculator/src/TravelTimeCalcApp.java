bc263579d158ebb08fc30599e7aee1523282ecfe
Josh Motta <joshua.a.motta@gmail.com>
2020-09-05 00:34:20 -0400

    Completed assignment

--- ch03_TravelTimeCalculator/src/TravelTimeCalcApp.java
+++ ch03_TravelTimeCalculator/src/TravelTimeCalcApp.java
@@ -16,21 +16,23 @@ public class TravelTimeCalcApp {
 		System.out.println("Welcome to the Travel Time Calculator");
 
 		do {
-
+			// Prompt user for input
 			System.out.print("\nEnter miles: ");
 			miles = scanner.nextDouble();
-
 			System.out.print("Enter miles per hour: ");
 			mph = scanner.nextDouble();
 
+			// Calculate hours and minutes
 			hours = (int) Math.floor(miles / mph);
-			minutes = (int) Math.floor(miles % mph);
+			minutes = (int) (((miles / mph) * 60) % 60);
 
+			// Output results
 			System.out.println("\nEstimated travel time");
 			System.out.println("---------------------");
 			System.out.println("Hours:   " + hours);
 			System.out.println("Minutes: " + minutes);
 
+			// Prompt user to continue
 			System.out.print("\nContinue? (y/n): ");
 			prompt = scanner.next();
 		} while (prompt.equalsIgnoreCase("y"));
