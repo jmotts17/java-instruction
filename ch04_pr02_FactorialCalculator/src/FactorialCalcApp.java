d883fed38ec6abc4432933dc47460181be3bafe7
Josh Motta <joshua.a.motta@gmail.com>
2020-09-13 16:18:33 -0400

    added comments

--- ch04_pr02_FactorialCalculator/src/FactorialCalcApp.java
+++ ch04_pr02_FactorialCalculator/src/FactorialCalcApp.java
@@ -14,17 +14,22 @@ public class FactorialCalcApp {
 		System.out.println("Welcome to the Factorial Calculator");
 
 		do {
+			// Prompt user for input
 			System.out.print("\nEnter an integer that's greater than 0 and less than 21: ");
 			int num = scanner.nextInt();
 
+			// Declare factorial variable
 			long factorial = 1;
 
+			// Calculate factorial
 			for (int i = 1; i <= num; i++) {
 				factorial *= i;
 			}
 
+			// Output results
 			System.out.println("The factorial of " + num + " is " + factorial);
 
+			// Prompt user to continue
 			System.out.print("\nContinue? (y/n): ");
 			choice = scanner.next();
 		} while (choice.equalsIgnoreCase("y"));
