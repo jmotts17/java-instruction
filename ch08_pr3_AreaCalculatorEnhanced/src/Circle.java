c6271ced40f3e4e8ce045f432b34d37d95dd9da4
Josh Motta <joshua.a.motta@gmail.com>
2020-09-26 13:46:47 -0400

    init commit

---
+++ ch08_pr3_AreaCalculatorEnhanced/src/Circle.java
@@ -0,0 +1,27 @@
+
+public class Circle implements Shape {
+
+	// Field
+	private double radius;
+
+	// Constructor
+	public Circle(double radius) {
+		this.radius = radius;
+	}
+
+	// Getter & Setter for radius
+	public double getRadius() {
+		return radius;
+	}
+
+	public void setRadius(double radius) {
+		this.radius = radius;
+	}
+
+	// Override of abstract method getArea
+	@Override
+	public double getArea() {
+		return Math.PI * (radius * radius);
+	}
+
+}
