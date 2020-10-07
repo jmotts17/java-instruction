4c1823630508497cb0b7fba704b0c16a1aa1b9a6
Josh Motta <joshua.a.motta@gmail.com>
2020-10-06 21:09:50 -0400

    Finished project

--- ch13_pr1_HTMLConverter/src/motta/app/HTMLConverterApp.java
+++ ch13_pr1_HTMLConverter/src/motta/app/HTMLConverterApp.java
@@ -4,16 +4,32 @@ public class HTMLConverterApp {
 
 	public static void main(String[] args) {
 
+		// Variable declaration
 		String html = "<h1>Grocery List</h1>\n" + "<ul>\n" + " <li>Eggs</li>\n" + " <li>Milk</li>\n"
 				+ " <li>Butter</li>\n" + "</ul>";
 
-		System.out.println("HTML Converter\n");
-		System.out.println("INPUT\n" + html);
+		// Output Welcome Message
+		System.out.println("HTML Converter");
 
+		// Output original String
+		System.out.println("\nINPUT");
+		System.out.println(html);
+
+		// Output new string without html tags
 		System.out.println("\nOUTPUT");
-		int openingTag = html.indexOf(">");
-		int closingTag = html.indexOf("</");
-		String title = html.substring(openingTag + 1, closingTag);
+		String pattern = "\\<.*?\\>";
+		String groceryList = html.replaceAll(pattern, "");
+		String[] groceryListArr = groceryList.split("\n");
+
+		for (int i = 0; i < groceryListArr.length; i++) {
+			if (groceryListArr[i].trim().isEmpty()) {
+				continue;
+			} else if (groceryListArr[i].contains("List")) {
+				System.out.println(groceryListArr[i]);
+			} else {
+				System.out.println("* " + groceryListArr[i].trim());
+			}
+		}
 
 	}
 }
