df710e1ae6838e45a6c5d51ee682ca11359fa6bb
Josh Motta <joshua.a.motta@gmail.com>
2020-10-31 13:24:56 -0400

    Added JUnit test

---
+++ ch07_pr2_GradeConverter/src/GradeTest.java
@@ -0,0 +1,18 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+class GradeTest {
+
+	@Test
+	void testLetterGradeA() {
+		Grade gradeA = new Grade(88);
+		assertEquals("A", gradeA.getLetter());
+	}
+
+	@Test
+	void testLetterGradeF() {
+		Grade gradeF = new Grade(50);
+		assertEquals("F", gradeF.getLetter());
+	}
+}
